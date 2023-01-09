826e7d91d247713c122a74de847a3891f29d44e1
rcph <rcph110110@gmail.com>
2023-01-09 22:00:53 +0800

    version1

---
+++ js/app.js
@@ -0,0 +1,63 @@
+const text_editor = document.querySelector(".text_editor")
+const log = document.querySelector(".log")
+const mode = document.querySelector(".mode")
+const startlog = document.querySelector(".startlog")
+const refresh = document.querySelector(".refresh")
+
+let logmode = 0
+
+// text_editor.addEventListener("input",async (e) => {
+// })
+if(localStorage.getItem("editor_cache")) {
+    text_editor.value = localStorage.getItem("editor_cache")
+}
+
+function echo(text) {
+    log.append(`${new Date().toLocaleTimeString("en-US",{hour12:false})} : ${text}`,document.createElement("br"))
+}
+
+function sleep(seconds) {
+    return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+}
+
+mode.addEventListener("click", () => {
+    logmode += 1
+    switch (logmode) {
+        case 1 : {
+            log.style.bottom = "-30vh"
+            mode.style.bottom = "50vh"
+            startlog.style.bottom = "50vh"
+            refresh.style.bottom = "50vh"
+        }
+        break
+        case 2 : {
+            log.style.bottom = "0"
+            mode.style.bottom = "80vh"
+            startlog.style.bottom = "80vh"
+            refresh.style.bottom = "80vh"
+        }
+        break
+        default : {
+            log.style.bottom = "-80vh"
+            mode.style.bottom = "0"
+            startlog.style.bottom = "0"
+            refresh.style.bottom = "0"
+            logmode = 0
+        }
+    }
+
+})
+startlog.addEventListener("click", () => {
+    //execute code
+    try {
+        eval(text_editor.value)
+    }
+    catch {
+
+    }
+    log.scrollTo(0,log.scrollHeight)
+})
+refresh.addEventListener("click",() => {
+    localStorage.setItem("editor_cache", text_editor.value)
+    location.reload()
+})
---
+++ readMe.md
@@ -0,0 +1,23 @@
+# ProCulator (What a Nice Name XD)
+### A Programming Calculator Created with HTML,CSS,JS
+
+###
+###
+###
+
+## Description
+- ProCulator Was Designed And Inspired by Programming And Calculating
+- While Calculator Has Limited Syntax, Well Programming Can Do A Lot!!!
+- So Much Stuff Can Do, Trig Functions, Create your own Function, Name Your Own Variable For Specified Session, Intervals, And Many More!!!
+- Enjoy!!! :)
+
+## Disadvantage
+
+- Can't Store Variable and Function If App/Site Has Been Terminated
+- There's no autocorrection/spellchecker so it's like c++ in notepad xd 
+* (i hope you get the joke, the joke is "c++ in notepad", did you get it?, you didn't?, aw nah, moving on)
+- only **javascript** is supported since i didn't knew what i have done.
+
+## Warning !!! 
+- ProCulator use "eval()" just to execute javascript code,"Be Cautious" :)
+- Also Spaghetti code (TnT)
